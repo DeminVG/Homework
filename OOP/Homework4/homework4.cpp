644239b9ed89bfacaf62bced4486c3860eb65fa3
Unknown <dvg30@yandex.ru>
2021-07-30 18:31:12 +0500

    add file homework4.cpp with lesson2

---
+++ OOP/Homework4/homework4.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    // Lesson 2
+    cout << "Lesson 2" << endl;
+    
+    vector<int> arr{12, 4, 56, 78, 4, 4, 12};
+    
+    vector<int>::iterator it;
+    it = arr. begin() ;
+    
+    sort(arr.begin(), arr.end()) ;
+    
+    int num = 0;
+    
+    for(int i = 0; i < arr.size(); i++){
+        if(*(it + i) != *(it + i +1)) {
+            num++;
+        }
+    } 
+    cout << "В векторе "<< num << " различных чисел(числа)." << endl;
+    
+    return 0;
+}
