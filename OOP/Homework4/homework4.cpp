b8e03fc63b37e9f642422beda90c96100598b4ee
Unknown <dvg30@yandex.ru>
2021-07-31 14:30:39 +0500

    add Lesson 1 to homework4.cpp add faile ArreyInt.h

--- OOP/Homework4/homework4.cpp
+++ OOP/Homework4/homework4.cpp
@@ -2,15 +2,34 @@
 #include <vector>
 #include <iterator>
 #include <algorithm>
+#include </home/demin/Desktop/OOP/ArrayInt.h>
 
 using namespace std;
 
 int main()
 {
-    setlocale(LC_ALL, "ru");
+    using namespace std;
+     // Lesson 1
+    {
+        cout << "Lesson 1:\n";
+        ArrayInt arr;
+        arr.push_back(2);
+        arr.push_back(14);
+        arr.push_back(5);
+        arr.push_back(71);
+        arr.push_back(4);
+        arr.push_back(13);
+        arr.PrintArr();
+
+        arr.pop_back();
+        arr.PrintArr();
+
+        arr.pop_front();
+        arr.PrintArr();
+    }
 
     // Lesson 2
-    cout << "Lesson 2" << endl;
+    cout << "\nLesson 2" << endl;
     
     vector<int> arr{12, 4, 56, 78, 4, 4, 12};
     
