ec9fd55c5ad60dde44104c45070bcbbec7fe01b7
Unknown <dvg30@yandex.ru>
2021-08-04 01:23:57 +0500

    add file homework6.cpp

---
+++ OOP/Homework6/homework6.cpp
@@ -0,0 +1,49 @@
+﻿
+#include <iostream>
+#include <ostream>
+#include <string>
+
+using namespace std;
+
+
+int input_int(const char* prompt_int)
+{
+    int a; 
+    cout << prompt_int; 
+    cin >> a;
+
+    while (cin.fail() || cin.get() != '\n')
+    {
+        cin.clear();
+        cin.ignore(9999, '\n');
+        cin.sync();
+        cerr << "Ошибка!\n\n" << prompt_int;
+        cin >> a;
+    }
+    return a;
+}
+
+ostream& endll(ostream& stream){
+    stream << "\n\n";
+    stream.flush();
+    return stream;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    // Lesson 1
+    {
+        cout << "Lesson 1:\n";
+    
+        cout << input_int("Введите число int: \n") << "\n\n";
+    }
+
+    // Lesson 2
+    {
+        cout << "Lesson 2:\n" << endll;
+        cout << "Два перевода строки и сброс буфера.\n";
+    }
+    return 0;
+}
